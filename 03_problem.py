83faad47081cccd228adc0e429d5d4318a331a20
qwa310 <wjdalsdkcla@naver.com>
2021-02-10 01:30:35 +0900

    Greedy Algorithm

---
+++ 03_problem.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    minvalue = min(data)
+    result = max(result, minvalue)
+    
+print(result)
+
+'''
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    minvalue = 10001
+
+    for a in data:
+        minvalue = min(minvalue, a)
+
+    result = max(result, minvalue)
+
+print(result)
+'''
---
+++ 04_problem.py
@@ -0,0 +1,51 @@
+n, k = map(int, input().split())
+count = 0
+
+while n >= k:
+    if n % k == 0:
+        n //= k
+        count += 1
+    else:
+        n -= 1
+        count += 1
+        
+while n > 1:
+    n -= 1
+    count += 1
+
+print(count)
+
+
+'''
+n, k = map(int, input().split())
+count = 0
+
+while n > 1:
+    target = (n // k) * k 
+    count += n - target     # n나누기k 했을 때의 나머지를 -1씩 해서 소거 => 나머지만큼 count함
+    n = target              # n이 k로 나누어 떨어지게 만든 target 값을 n에 넣음
+    if n < k:
+        count += (n - 1)
+        n = 1
+    count += 1              # n을 k로 나눌 때의 count
+    n //= k
+
+print(count)
+'''
+
+'''
+n, k = map(int, input().split())
+count = 0
+
+while True:
+    target = (n // k) * k 
+    count += n - target     # n나누기k 했을 때의 나머지를 -1씩 해서 소거 => 나머지만큼 count함
+    n = target              # n이 k로 나누어 떨어지게 만든 target 값을 n에 넣음
+    if n < k:
+        break
+    count += 1              # n을 k로 나눌 때의 count
+    n //= k
+
+count += (n - 1)
+print(count)
+'''
